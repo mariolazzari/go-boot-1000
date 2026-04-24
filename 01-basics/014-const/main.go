e8e060175d4bad22cd95f8dc12e6c7ef46e2bc09
Mario Lazzari <mario.lazzari@gmail.com>
2026-04-24 23:23:27 +0200

    Done lesson 14

--- 01-basics/014-const/main.go
+++ 01-basics/014-const/main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	const (
+		monday = iota + 1
+		tuesday
+		wednesday
+		thursday
+		firday
+		saturday
+		sunday
+	)
+
+	fmt.Println(monday, tuesday, wednesday, thursday, firday, saturday, sunday)
+
+	const (
+		EST = -(iota + 5)
+		_
+		MST
+		PST
+	)
+
+	fmt.Println(EST, MST, PST)
 }
